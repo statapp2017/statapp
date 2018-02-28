84a3023b29b0cd6162eaf0c25d3c28856acaf9f0
statapp2017 <33500919+statapp2017@users.noreply.github.com>
2018-02-28 01:31:00 +0100

    Update sentiment_analysis_xgboost.R

--- sentiment_analysis_xgboost.R
+++ sentiment_analysis_xgboost.R
@@ -6,11 +6,11 @@ library(xgboost)
 library(ggplot2)
 library(tm)
 library(MLmetrics)
-#install.packages("foreach")
+#install.packages("foreach",dependencies = TRUE)
 library(foreach)
-#install.packages("doParallel")
 library(doParallel)  
 library(dplyr)
+library(iterators)
 
 gestion_dtm<-function(dtm){
   mots_sentiment<-FEEL[,"word"]
@@ -36,13 +36,6 @@ gestion_dtm<-function(dtm){
   dtm_final    
 }
 
-#load test results
-setwd("D:/ENSAE/2emeannee/Statsapp")
-table<-read.csv('tableau_calibrage.csv')
-dtm_ep <- readRDS(file = "dtm_epargne.RDS")
-verbatim <- read.csv2("verbatims_SRC_230118_ENSAE.csv", encoding = "latin1")
-epargne <- subset(verbatim, REPRISE_ACTIVITE == "Epargne")
-
 creation_class<-function(x,number_class){
   if (number_class=="2"){
     if (x <6){
@@ -66,11 +59,11 @@ creation_class<-function(x,number_class){
   x
 }
 
-get_data_xgboost<-function(dtm_ep,train_test_split=.8,train_valid_split=.8){
+get_data_xgboost<-function(dtm_ep,data,number_class,train_test_split=.8,train_valid_split=.8){
   d1 <- as.data.frame(as.matrix(dtm_ep))
-  data_tot<-cbind(epargne$recommandation_SGK,d1)
+  data_tot<-cbind(data$recommandation_SGK,d1)
   colnames(data_tot)<-c("recommandation_SGK",colnames(d1))
-  data_tot$recommandation_SGK<-sapply(data_tot$recommandation_SGK,FUN=function(x){creation_class(x,3)})
+  data_tot$recommandation_SGK<-sapply(data_tot$recommandation_SGK,FUN=function(x){creation_class(x,number_class)})
   sample1 = sample.int(n = nrow(data_tot), size = floor(train_test_split*nrow(data_tot)), replace = F)
   train<-data_tot[sample1,]
   test<-data_tot[-sample1,]
@@ -80,26 +73,26 @@ get_data_xgboost<-function(dtm_ep,train_test_split=.8,train_valid_split=.8){
   dtrain<-xgb.DMatrix(data = as.matrix(train_t[,2:ncol(data_tot)]),label = as.matrix(train_t[,1])) 
   dvalid<-xgb.DMatrix(data = as.matrix(valid[,2:ncol(data_tot)]),label = as.matrix(valid[,1]))
   watchlist = list(train = dtrain, valid = dvalid)
-  list(test,watchlist)
+  list(test,watchlist,sample2)
 }
 
 search_best_config<-function(number_class,dtm_ep){
   eta_par <- 0.1
   nrounds_par <- 5 / eta_par
   # XGB.TRAIN WITH PREVIOUS PARAMETER + test on eta 
-  watchlist<-get_data_xgboost(dtm_ep)[[2]]
+  watchlist<-get_data_xgboost(dtm_ep,number_class)[[2]]
   dtrain<-watchlist$train
   Logloss_list<-c()
   param<-data.frame(eta=c(),max_depth=c(),subsample=c(),colsample_bytree=c(),objective=c(),eval_metric=c(),num_class=c(),nthread=c(),tree_method=c(),silent=c())
   for(depth_par in seq(6, 14, by = 2)) {
     for(subsample_par in seq(0.1, 1, by = 0.1)) {
       for(colsample_bytree_par in seq(0.1, 1, by = 0.1)) {
-          bstSparse <- xgb.train(data = dtrain,eta = eta_par, max_depth = depth_par, subsample = subsample_par,
-                                     colsample_bytree = colsample_bytree_par,objective = "multi:softmax",eval_metric="mlogloss",
-                                 num_class = number_class,nrounds = nrounds_par,nthread = 2, tree_method = "auto", 
-                                 watchlist = watchlist,verbose=0,early_stopping_rounds=50)
-          Logloss_list<-append(Logloss_list,bstSparse$best_score)
-          param<-rbind(param,bstSparse$params)
+        bstSparse <- xgb.train(data = dtrain,eta = eta_par, max_depth = depth_par, subsample = subsample_par,
+                               colsample_bytree = colsample_bytree_par,objective = "multi:softmax",eval_metric="mlogloss",
+                               num_class = number_class,nrounds = nrounds_par,nthread = 2, tree_method = "auto", 
+                               watchlist = watchlist,verbose=0,early_stopping_rounds=50)
+        Logloss_list<-append(Logloss_list,bstSparse$best_score)
+        param<-rbind(param,bstSparse$params)
       }
     }
   }
@@ -110,35 +103,48 @@ search_best_config<-function(number_class,dtm_ep){
 #t<-search_best_config(3,dtm_ep)
 
 #write.csv2(params[,2:ncol(params)],"logloss.csv",row.names = FALSE)
-
-# XGB.TRAIN WITH PREVIOUS PARAMETER + test on eta 
 parameters<-read.csv2("logloss.csv")
-predictor<-funtion(params,dtm_ep){
-  eta<-params[params$logloss==min(params$logloss),2:ncol(params)]$eta
-  xgb.train(data=dtrain,nrounds=5/eta,t[t$logloss==min(t$logloss),2:ncol(t)])
-  data<-get_data_xgboost(dtm_ep)
-  watchlist<-data[[2]]
-  dtrain<-watchlist$train
-  test<-data[[1]]
-}
 params<-parameters[parameters$logloss==min(parameters$logloss),2:ncol(parameters),]
-t<-get_data_xgboost(dtm_ep,train_test_split=1,train_valid_split=.8)[[2]]$train
 
-bagging<-function(params,dtm_ep,length_divisor=4,iterations=5){
-  cl <- detectCores() %>% -1 %>% makeCluster
-  registerDoParallel(cl)  
-  dtest<-get_data_xgboost(dtm_ep,train_test_split=1,train_valid_split=.8)[[2]]$valid
-  dtrain<-watchlist$train
-  eta<-params$eta
+bagging<-function(data,column,params,dtm_ep,number_class,length_divisor=4,iterations=2){
+  #cl <- makeCluster(3)
+  #clusterExport(cl,c("dtrain","dtest","eta","params"))
+  #registerDoParallel(cl)
+  label<-sapply(data[,column],FUN=function(x){creation_class(x,number_class)})
   predictions<-foreach(m=1:iterations,.packages=c('xgboost'),.combine=cbind) %do% {
-    training_positions <- sample(nrow(dtrain), size=floor((nrow(dtrain)/length_divisor)))
+    data_tot<-get_data_xgboost(dtm_ep,data,number_class,train_test_split=1,train_valid_split=.8)
+    watchlist<-data_tot[[2]]
+    new_sample<-data_tot[[3]]
+    dtest<-watchlist$valid
+    dtrain<-watchlist$train
+    eta<-params$eta
+    training_positions <- sample(nrow(dtrain), size=floor((nrow(dtrain)/2)))
     sub_dtrain<-dtrain[training_positions,]
     bstSparse <- xgb.train(data=sub_dtrain,nrounds=5/eta,params,watchlist = list(train=sub_dtrain,valid=dtest),verbose=0,early_stopping_rounds=50)
+    importance<-xgb.importance(colnames(dtm_ep),model = bstSparse,data=as.matrix(dtm_ep)[new_sample,],label=data[new_sample,column] )
+    View(importance)
+    print(xgb.ggplot.importance(importance_matrix = importance,top_n = 30,n_clusters = 1))
     predict(bstSparse,newdata=dtest)
   }
-  num_class<-(params$num_class-1)
-  for (i in 0:num_class){
-    for (j in 1:nrow(predictions))
+  for (col in 1:ncol(predictions)){
+    predictions[,col]<-sapply(predictions[,col],as.character)
   }
+  num_class<-(params$num_class-1)
+  as.data.frame(predictions)
+  prediction<-c()
+  for (j in 1:nrow(predictions)){
+    pred<-0
+    max_sum<-sum(ifelse(predictions[j,]=="0", 1, 0))
+    for (i in 1:num_class){
+      sum_inter<-sum(ifelse(predictions[j,]==as.character(i), 1, 0))
+      if (sum_inter>max_sum){
+        pred<-i
+        max_sum<-sum_inter
+      }
+    }
+    prediction[length(prediction)+1]<-pred
+  }
+  prediction
 }
-t<-bagging(params,dtm_ep)
+
+predictions<-bagging(so_ge_prevoyance,"recommandation_SGK",params,dtm_ep,3)
