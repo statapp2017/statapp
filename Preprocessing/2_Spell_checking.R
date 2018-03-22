226559a5b7d1e1b8cecb45b5fc81fa90adac7e23
statapp2017 <33500919+statapp2017@users.noreply.github.com>
2018-03-22 18:20:40 +0100

    Update 2_Spell_checking.R

--- Preprocessing/2_Spell_checking.R
+++ Preprocessing/2_Spell_checking.R
@@ -1,7 +1,6 @@
 library(tokenizers)
 library(hunspell)
 
-
 #' Transform a sentence in a given \code{file} to lemmatize all the words
 #' and find also the grammatical class of all words. This algorithm is based
 #' on TreeTagger
@@ -13,46 +12,41 @@ library(hunspell)
 #' dataframe_correction("Myfile.txt","C:/TreeTagger")
 
 
-
 #' Correct the spelling of a given sentence.
 #' 
-#' @param sentence a french string
+#' @param sentence a string in French
 #' @return The correction of \code{sentence}.
 #' @examples
 #' spell_checker("Il pleut aujourd'hui.")
 #' spell_checker("Je sui contre ce projet.")
 
-
-spell_checker<- function(sentence){
-  #Tokenization of the sentence 
-  list_words <- tokenize_words(as.character(gsub(" \n", ". ",sentence)), lowercase = TRUE)[[1]]
-  string_correct<- ""
-  for (x in list_words){
-    #Check if the spelling of the words is correct
-    if (hunspell_check(x,dict=dictionary("fr"))|str_length(x)<3){
-      string_correct<- paste(string_correct,x)
-    }
-    else{
-      #If the spelling isn't accurate we recover hunspell suggestions
+spell_checker <- function(sentence) {
+  # Tokenization of the sentence 
+  list_words <- tokenize_words(as.character(gsub(" \n", ". ", sentence)), lowercase = TRUE)[[1]]
+  correct_string <- ""
+  for (x in list_words) {
+    # Check if the spelling of the words is correct
+    if (hunspell_check(x, dict = dictionary("fr"))|str_length(x) < 3) {
+      correct_string <- paste(correct_string,x)
+    } else {
+      # If the spelling isn't accurate we recover hunspell suggestions
       suggestion<-hunspell_suggest(x, dict = dictionary("fr"))[[1]]
       stop <- TRUE
       i <- 1
-      while (stop & i<3){
-        #We check that the word is french before we accept the change
+      while (stop & i < 3) {
+        # We check that the word is French before we accept the change
         if (suggestion[i] %in% dico[[1]]){
-          string_correct <- paste(string_correct,suggestion[i])
+          correct_string <- paste(correct_string, suggestion[i])
           stop <- FALSE
-        }
-        else{
-          i <- i+1
+        } else {
+          i <- i + 1
         }
       }
-      #If the algorithm isn't able to find a good spelling we let the way it was
-      if (i==5){
-        print(1)
-        string_correct <- paste(string_correct,x)
+      # If the algorithm isn't able to find a good spelling, we leave it as it was
+      if (i == 5) {
+        correct_string <- paste(correct_string, x)
       }
     }  
   }
-  string_correct
+  correct_string
 }
