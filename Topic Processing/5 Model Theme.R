5c99f060ca6dddc1c35ba0fb14a8cd5a9e8a2dca
statapp2017 <33500919+statapp2017@users.noreply.github.com>
2018-03-26 19:27:50 +0200

    Update 5 Model Theme.R

--- Topic Processing/5 Model Theme.R
+++ Topic Processing/5 Model Theme.R
@@ -4,20 +4,20 @@ library(topicmodels)
 library(ggplot2)
 library(magrittr)
 
-## ANALYSE THEMATIQUE ##
+## THEMATIC ANALYSIS ##
 give_theme <- function(dtm) {
-  # Liste des themes attribuables a chaque document
+  # List of topics attributable to each document
   best_model <- give_best_model(dtm)
   print(best_model)
   document_topic_assignments <- get_topic_assignments(best_model) 
   Topic <- topics(best_model, 1)     
-  # Probabilite de chaque mot d'appartenir a un theme                                            
+  # Probability of each word belonging to a topic                                   
   pos <- posterior(best_model)$topics   
   phi_t <- posterior(best_model)$terms %>% as.matrix  
-  # Frequence des themes 
+  # Frequency of topics
   topic_freqs <- sort(table(unlist(document_topic_assignments$topic)), decreasing = T) 
   # MDS afin d'analyser la distance entre les themes                                              
-  #topic_dist<-dist_topic (phi_t)                                                              
+  #topic_dist <- dist_topic (phi_t)                                                              
   #Sur le graphique sont affiches les numeros designant chaque theme,                            
   #les themes devant etre interpretes avec la figure obtenue avec la fonction caracterise       
   return(list(topics_freq = topic_freqs, phi_t = phi_t, theta = pos,
