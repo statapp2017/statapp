660dcbda776723c76e69a9fc7d306984ceed9d89
statapp2017 <33500919+statapp2017@users.noreply.github.com>
2018-03-26 19:30:55 +0200

    Update 3 TSNE.R

--- Topic Processing/3 TSNE.R
+++ Topic Processing/3 TSNE.R
@@ -1,7 +1,7 @@
 library(RColorBrewer)
 library(Rtsne)
 
-## ANALYSE THEMATIQUE ##
+## THEMATIC ANALYSIS ##
 try_tsne <- function(phi) {
   train <- as.data.frame(t(phi))
   Labels <- c()
@@ -10,14 +10,14 @@ try_tsne <- function(phi) {
     Labels[i] <- which(train[rown[i], ] == max(train[rown[i], ]))
   }
   Labels <- as.factor(Labels)
-  colors = brewer.pal(length(unique(colnames(train))), "Paired")
+  colors <- brewer.pal(length(unique(colnames(train))), "Paired")
   lab <- unique(Labels)
   names(colors) <- lab
   legends <- c()
-  for (i in 1:length(unique(Labels))){
+  for (i in 1:length(unique(Labels))) {
     legends[i] <- paste("Thème", as.character(lab[i]))
   }
   tsne <- Rtsne(train, dims = 2, perplexity = 30, verbose = TRUE, max_iter = 500, check_duplicates = FALSE)
-  plot(tsne$Y, t = 'p',pch = 21, main = "tsne", col = colors[Labels], bg = colors[Labels])
-  legend("bottomleft", legend=legends, col = colors[unique(Labels)], pt.bg=colors[unique(Labels)],pch = rep(21,length(Labels)))
+  plot(tsne$Y, t = "p", pch = 21, main = "tsne", col = colors[Labels], bg = colors[Labels])
+  legend("bottomleft", legend = legends, col = colors[unique(Labels)], pt.bg = colors[unique(Labels)], pch = rep(21, length(Labels)))
 }
