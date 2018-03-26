81da7c73e581878478150312d597ce90bda0218b
statapp2017 <33500919+statapp2017@users.noreply.github.com>
2018-03-26 19:45:27 +0200

    Update 1 Number of themes.R

--- Topic Processing/1 Number of themes.R
+++ Topic Processing/1 Number of themes.R
@@ -26,32 +26,31 @@ test_convergence <- function(dtm, n) {
   liste <- c()
   for (i in 1:n) {
     liste[i] <- LDA(subset(as.matrix(dtm), (rowSums(as.matrix(dtm)) > 0) == TRUE), k = 5, method = "Gibbs", 
-                    control = list(burnin = 500, thin = 10, iter = 1500,keep = 50))@loglikelihood
+                    control = list(burnin = 500, thin = 10, iter = 1500, keep = 50))@loglikelihood
   }
   boxplot(liste)
   plot(liste, ylim = c(min(liste) - 10, max(liste) + 10))
-  par(mfrow = c(1, 1))
   sd(liste)
 }
 
 #test_convergence(dtm_ep, 100)
 
-coherence_tfidf <- function(model, dtm, n) { # n = number of topics in the model considered
-  dtm2 <- as.matrix(weightSMART(dtm,spec="atn"))
+coherence_tfidf <- function(model, dtm, n) {# n = number of topics in the model considered
+  dtm2 <- as.matrix(weightSMART(dtm, spec = "atn"))
   dtm <- as.matrix(dtm)
   phi_t <- posterior(model)$terms %>% as.matrix
   liste_score <- c()
   for (topics in 1:n) {# n -> number of topics
-    col_tri <- sort(phi_t[topics,],decreasing = T)
+    col_tri <- sort(phi_t[topics, ], decreasing = T)
     # We calculate the metric over the 10 best words of the topics
     w <- col_tri[1:10]
     u <- names(w)
     somme <- 0
-    for (i in 1:(length(u)-1)) {
-      for (j in (i+1):length(u)) {
-        sub_dtm <- dtm[dtm[,u[i]] > 0 & dtm[, u[j]] > 0, ]
-        high_sum <- sum(dtm2[rownames(sub_dtm), u[i]]*dtm2[rownames(sub_dtm), u[j]]) + 1
-        low_sum <- sum(dtm[,u[i]])
+    for (i in 1:(length(u) - 1)) {
+      for (j in (i + 1):length(u)) {
+        sub_dtm <- dtm[dtm[, u[i]] > 0 & dtm[, u[j]] > 0, ]
+        high_sum <- sum(dtm2[rownames(sub_dtm), u[i]] * dtm2[rownames(sub_dtm), u[j]]) + 1
+        low_sum <- sum(dtm[, u[i]])
         somme <- somme + log(high_sum / low_sum)
       }
     }
@@ -77,32 +76,31 @@ get_best_model <- function(all_topics_models, dtm) {
   for(i in 1:Nombre_de_k) {
     average_coherences[i] <- moyenne(all_coherences, i)
   }
-  data <- data.frame(x=2:10, y=average_coherences)
-  choix <- list(
+  data <- data.frame(x = 2:10, y=average_coherences)
+  choice <- list(
     x = (which(average_coherences == max(average_coherences))+1),
-    y =(min(average_coherences)-1),
+    y = (min(average_coherences)-1),
     text = ~paste("Nombre de thèmes choisis :", as.character((which(average_coherences == max(average_coherences))+1))),
     font = list(family = 'Arial', size = 16, color = 'rgba(49,130,189, 1)'),
     showarrow = FALSE)
-  p <- plot_ly(data, x = ~x, y = ~y,name="Cohérence tf-idf moyenne", type = 'scatter', mode = 'lines')%>%
-    add_trace(x= ~(which(average_coherences == max(average_coherences))+1), name="Nombre de thèmes choisis", line = list(color = 'rgb(22, 96, 167)', width = 4))%>%
-    layout(title = "Cohérence tf-idf moyenne en fonction du nombre de thèmes",
-           xaxis = list(title = "Nombre de thèmes"),
-           yaxis = list(title = "Cohérence tf-idf moyenne"))%>%layout(annotations=choix)%>%
-    add_trace(x = ~(which(average_coherences == max(average_coherences))+1), y = ~average_coherences[which(average_coherences ==max(average_coherences))],name="Cohérence tf-idf maximale", type = 'scatter', mode = 'markers', marker = list(color = 'rgba(67,67,67,1)', size = 8)) 
+  p <- plot_ly(data, x = ~x, y = ~y, name = "Cohérence tf-idf moyenne", type = "scatter", mode = "lines")%>%
+    add_trace(x= ~(which(average_coherences == max(average_coherences)) + 1), name="Nombre de thèmes choisis", line = list(color = 'rgb(22, 96, 167)', width = 4))%>%
+    layout(title = "Cohérence tf-idf moyenne en fonction du nombre de thèmes", xaxis = list(title = "Nombre de thèmes"),
+           yaxis = list(title = "Cohérence tf-idf moyenne"))%>%layout(annotations = choice)%>%
+    add_trace(x = ~(which(average_coherences == max(average_coherences)) + 1), y = ~average_coherences[which(average_coherences == max(average_coherences))], 
+              name = "Cohérence tf-idf maximale", type = 'scatter', mode = 'markers', marker = list(color = 'rgba(67,67,67,1)', size = 8)) 
   print(p)
-  #BEST MODEL :
-  best_model<-all_topics_models[[as.character(which(average_coherences ==max(average_coherences))+1)]]
-  best_model
+  # BEST MODEL :
+  all_topics_models[[as.character(which(average_coherences == max(average_coherences)) + 1)]]
 }
 
 give_best_model <- function(dtm) {                                                                
-  # Nombre de themes testes                                                                      
+  # Number of tested topics                                                              
   all_ks <- seq(2, 10, 1)                                                                        
   # Matrice dont les documents (lignes) contiennent au moins un terme (colonne)                  
-  documents <- subset(as.matrix(dtm),(rowSums(as.matrix(dtm)) >0) ==TRUE)                          
-  # Nombre optimal de theme via LDA avec Gibbs                                                   
+  documents <- subset(as.matrix(dtm), (rowSums(as.matrix(dtm)) > 0) == TRUE)                          
+  # Best number of topics via LDA with Gibbs                                                   
   all_topic_models <- build_topic_models(documents, all_ks)
-  # Modele selectionne
+  # Selected model
   get_best_model(all_topic_models, dtm) 
 }      
