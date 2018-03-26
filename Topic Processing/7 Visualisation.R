7485305224b42ff3509422af2198192e838aa070
statapp2017 <33500919+statapp2017@users.noreply.github.com>
2018-03-26 20:10:58 +0200

    Update 7 Visualisation.R

--- Topic Processing/7 Visualisation.R
+++ Topic Processing/7 Visualisation.R
@@ -1,18 +1,27 @@
 library(LDAvis)
 
-phi_v <- model_theme$phi_t # Probability that a word belongs to a topic (ok)
-theta_v <- model_theme$theta # Probability that a document belongs to a topic (ok)
-doc.length_v <- as.data.frame(table(dtm_ep$i))[2]$Freq # Number of words per document (ok)
-vocab_v <- dtm_ep$dimnames$Terms # Words list
-vocab_v[92] <- "fa"
-vocab_v[93] <- "re"
-term.frequency_v <- as.data.frame(table(dtm_ep$j))[2]$Freq # Fréquence des mots dans tout le document"
+#' Visualise dynamically the results of the topic processing with LDA thanks to LDAvis.
+#' 
+#' @param model The LDA model, output of \code{\link{give_theme}}.
+#' @param dtm The document-term matrix obtained after the preprocessing.
+#' @return An interactive visualisation of the topic processing.
+#' @examples
+#' visualise_LDA(model_theme, dtm_ep)
 
-list_themes <- list(phi = phi_v, theta = theta_v, doc.length = doc.length_v, vocab = vocab_v, 
-                    term.frequency = term.frequency_v)
 
-json <- createJSON(phi = list_themes$phi, theta = list_themes$theta, 
-                   doc.length = list_themes$doc.length, vocab = list_themes$vocab, 
-                   term.frequency = list_themes$term.frequency)
+visualise_LDA <- function(model, dtm) {
+  #model <- give_theme(dtm)
+  phi <- model$phi_t # Probability that a word belongs to a topic (ok)
+  theta <- model$theta # Probability that a document belongs to a topic (ok)
+  doc.length <- as.data.frame(table(dtm$i))[2]$Freq # Number of words per document (ok)
+  vocab <- dtm$dimnames$Terms # Words list
+  vocab[92] <- "fa"
+  vocab[93] <- "re"
+  term.frequency <- as.data.frame(table(dtm$j))[2]$Freq # Fréquence des mots dans tout le document"
+  json <- createJSON(phi = phi, theta = theta, doc.length = doc.length, vocab = vocab, 
+                     term.frequency = term.frequency)
+  serVis(json, out.dir = "vis", open.browser = TRUE)
+}
 
-serVis(json, out.dir = 'vis', open.browser = TRUE)
+visualise_LDA(model_theme, dtm_ep)
+
