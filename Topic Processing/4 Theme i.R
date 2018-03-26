7c24b6bef131ef3565041fe25a5ffcf7b287ab19
statapp2017 <33500919+statapp2017@users.noreply.github.com>
2018-03-26 19:03:18 +0200

    Update 4 Theme i.R

--- Topic Processing/4 Theme i.R
+++ Topic Processing/4 Theme i.R
@@ -12,18 +12,17 @@ caracterise <- function(phi_t, topics_freqs, donnees) {
   p2 <- ggplotly(bp)
   print(p2)
   df <- as.data.frame(t(phi_t))
-  df$nom<-rownames(df)
+  df$name <- rownames(df)
   col <- list()
-  for (i in (1:(ncol(df)-1))) {
-    titre <- paste("Thème", as.character(i), sep = "")
-    col[i]<-titre
+  for (i in (1:(ncol(df) - 1))) {
+    col[i] <- paste("Thème", as.character(i), sep = "")
   }
-  col[length(col)+1] <- "nom"
+  col[length(col) + 1] <- "nom"
   colnames(df) <- col
   a_garder <- c()
   tags <- get_all_tags(donnees)
-  a_eliminer <- tags[tags$tags %in% c("ADJ", "KON", "PRP", "PUN", "DET", "PRO", "ADV","NUM"), 1]
-  df <- df[!df$nom %in% a_eliminer, ]
+  a_eliminer <- tags[tags$tags %in% c("ADJ", "KON", "PRP", "PUN", "DET", "PRO", "ADV", "NUM"), 1]
+  df <- df[!df$name %in% a_eliminer, ]
   for (i in (1:(ncol(df)-1))) {
     df <- df[order(df[, i], decreasing = T), ]
     df <- df[!rownames(df)%in%hash_valence$x, ]
@@ -36,8 +35,8 @@ caracterise <- function(phi_t, topics_freqs, donnees) {
   }
   df <- df[a_garder, ]
   df <- df[order(df[, which(colnames(df) == "theme")], decreasing = F), ]
-  df$nom <- factor(df$nom, levels = df$nom)
-  p <- ggplot(data = df, aes(x = nom, y = valeur, color = theme)) + geom_bar(stat = "identity") + 
+  df$name <- factor(df$name, levels = df$name)
+  p <- ggplot(data = df, aes(x = name, y = valeur, color = theme)) + geom_bar(stat = "identity") + 
     labs(title = "Mots illustrant les différents thèmes", x = "Mots", 
          y = "Probabilité") + theme(axis.text.x = element_text(angle = 45))
   p1 <- ggplotly(p)
