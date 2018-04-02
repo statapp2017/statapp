19132349136f99d9a4820452bfa07f5fa0bf7ffb
statapp2017 <33500919+statapp2017@users.noreply.github.com>
2018-04-02 12:08:53 +0200

    Update 3 TSNE.R

--- Topic Processing/3 TSNE.R
+++ Topic Processing/3 TSNE.R
@@ -3,22 +3,22 @@ library(Rtsne)
 
 ## THEMATIC ANALYSIS ##
 #phi = distribution of words by docuements (matrix)
-try_tsne <- function(phi) {
-  train <- as.data.frame(t(phi))
-  Labels <- c()
+try_tsne<-function(phi){
+  train<-as.data.frame(t(phi))
+  Labels<-c()
   rown <- rownames(train)
-  for (i in 1:nrow(train)) {
-    Labels[i] <- which(train[rown[i], ] == max(train[rown[i], ]))
+  for (i in 1:nrow(train)){
+    Labels[i] <- which(train[rown[i],]==max(train[rown[i],]))
   }
-  Labels <- as.factor(Labels)
-  colors <- brewer.pal(length(unique(colnames(train))), "Paired")
-  lab <- unique(Labels)
+  Labels<-as.factor(Labels)
+  colors =brewer.pal(length(unique(colnames(train))),"Paired")
+  lab<-unique(Labels)
   names(colors) <- lab
-  legends <- c()
-  for (i in 1:length(unique(Labels))) {
-    legends[i] <- paste("Thème", as.character(lab[i]))
+  legends<-c()
+  for (i in 1:length(unique(Labels))){
+    legends[i]<-paste("Thème",as.character(lab[i]))
   }
-  tsne <- Rtsne(train, dims = 2, perplexity = 30, verbose = TRUE, max_iter = 500, check_duplicates = FALSE)
-  plot(tsne$Y, t = "p", pch = 21, main = "tsne", col = colors[Labels], bg = colors[Labels])
-  legend("bottomleft", legend = legends, col = colors[unique(Labels)], pt.bg = colors[unique(Labels)], pch = rep(21, length(Labels)))
+  tsne <- Rtsne(train, dims = 2, perplexity=30, verbose=TRUE, max_iter = 500,check_duplicates = FALSE)
+  plot(tsne$Y, t='p',pch=21, main="Tsne mots",col=colors[Labels],bg=colors[Labels],xlab="Axe 1",ylab="Axe 2")
+  legend("bottomleft",legend=legends,col=colors[unique(Labels)],pt.bg=colors[unique(Labels)],pch = rep(21,length(Labels)))
 }
