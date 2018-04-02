19146dd774425dc80541a23907a82addbc1fa521
statapp2017 <33500919+statapp2017@users.noreply.github.com>
2018-04-02 12:10:24 +0200

    Update 4 Theme i.R

--- Topic Processing/4 Theme i.R
+++ Topic Processing/4 Theme i.R
@@ -3,44 +3,35 @@ library(ggplot2)
 
 ## ANALYSE THEMATIQUE ##
 # Characterize the i-th topic
-# topics_freq = table of frenquency of topics by documents
-# imput_data = the data frame containing the verbatims
-caracterise <- function(phi_t, topics_freqs, imput_data) {
-  freq <- as.matrix(topics_freqs)
-  freq2 <- as.data.frame(freq)
-  freq2$theme <- paste("Thème", rownames(freq2))
-  bp <- ggplot(freq2, aes(x = theme, y = V1)) + geom_bar(stat = "identity") + 
-    labs(title = "Nombre de documents par thème", x = "Thème", y = "Nombre de document")
-  p2 <- ggplotly(bp)
-  print(p2)
+# a_eliminer= the words associated with a strong sentiment by xgboost
+
+caracterise<-function(phi_t,a_eliminer){
   df <- as.data.frame(t(phi_t))
-  df$name <- rownames(df)
-  col <- list()
-  for (i in (1:(ncol(df) - 1))) {
-    col[i] <- paste("Thème", as.character(i), sep = "")
+  df$nom<-rownames(df)
+  col<-list()
+  for (i in (1:(ncol(df)-1))){
+    titre <-paste("Theme",as.character(i),sep="")
+    col[i]<-titre
   }
-  col[length(col) + 1] <- "nom"
-  colnames(df) <- col
-  to_keep <- c()
-  tags <- get_all_tags(imput_data)
-  to_delete <- tags[tags$tags %in% c("ADJ", "KON", "PRP", "PUN", "DET", "PRO", "ADV", "NUM"), 1]
-  df <- df[!df$name %in% to_delete, ]
-  for (i in (1:(ncol(df)-1))) {
-    df <- df[order(df[, i], decreasing = T), ]
-    df <- df[!rownames(df)%in%hash_valence$x, ]
-    to_keep <- c(to_keep, c(rownames(df[1:10, ])))
-    ref <- df[, i]
-    for (j in 1:10) {
-      df$theme[j] <- paste("Thème", as.character(i))
-      df$valeur[j] <- ref[j]
+  col[length(col)+1]<-"nom"
+  colnames(df)<-col
+  df<- df[!df$nom %in% a_eliminer,]
+  a_garder<-c()
+  for (i in (1:(ncol(df)-1))){
+    df<-df[order(df[,i],decreasing=T),]
+    a_garder<-c(a_garder,c(rownames(df[1:10,])))
+    ref <- df[,i]
+    for (j in 1:10){
+    df$theme[j]<-paste("Thème",as.character(i))
+    df$valeur[j]<-ref[j]
     }
   }
-  df <- df[to_keep, ]
-  df <- df[order(df[, which(colnames(df) == "theme")], decreasing = F), ]
-  df$name <- factor(df$name, levels = df$name)
-  p <- ggplot(data = df, aes(x = name, y = valeur, color = theme)) + geom_bar(stat = "identity") + 
-    labs(title = "Mots illustrant les différents thèmes", x = "Mots", 
-         y = "Probabilité") + theme(axis.text.x = element_text(angle = 45))
-  p1 <- ggplotly(p)
+  df<-df[a_garder,]
+  df <- df[order(df[,which(colnames(df)=="theme")],decreasing=F),]
+  df$nom<-factor(df$nom, levels=df$nom)
+  p<-ggplot(data=df,aes(x=nom,y=valeur,color=theme)) +
+    geom_bar(stat="identity")+labs(title="Mots illustrant les différents thèmes",
+                                  x ="Mots", y = "Probabilité")+theme(axis.text.x = element_text(angle=45))
+  p1<-ggplotly(p)
   print(p1)
 }
