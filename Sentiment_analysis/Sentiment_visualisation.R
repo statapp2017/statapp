7e04af8ffc7e60efd7d59af7c5465458f89f7617
statapp2017 <33500919+statapp2017@users.noreply.github.com>
2018-04-03 14:25:25 +0200

    Update Sentiment_visualisation.R

--- Sentiment_analysis/Sentiment_visualisation.R
+++ Sentiment_analysis/Sentiment_visualisation.R
@@ -3,6 +3,11 @@ setwd("D:/ENSAE/2emeannee/Statsapp")
 source("lime-master/R/explain.R")
 source("lime-master/R/dataframe.R")
 
+#' Gives a visual explanation on the verbatims classification. The algorithm
+#' is based on lime slightly modified to take into account the bagging
+#' @param new_data The Document Term Matrix of the verbatim to work on
+#' It's necessary to transform it with gestion_dtm before
+#' @param list_models The list of xgb.train models used for the prediction
 
 sentiments_results_visualisation<-function(new_data,list_models){
   new_data<-as.data.frame(new_data)
