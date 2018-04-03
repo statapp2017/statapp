374c9d7d423d9f068d4f429cb4259b3076ba8efe
statapp2017 <33500919+statapp2017@users.noreply.github.com>
2018-04-03 13:43:04 +0200

    Update Analyse new_verbatim.R

--- Sentiment_analysis/Analyse new_verbatim.R
+++ Sentiment_analysis/Analyse new_verbatim.R
@@ -2,6 +2,12 @@ library(topicmodels)
 source("Sentiment_analysis/Bagging.R")
 source("5_Create DTM.R")
 
+#' Gives the probabilities of each observation to be in a subject and in a class of 
+#' sentiment
+#' @param new_verbatim A list of new verbatims to analyse.
+#' @return A list which contains the probability of each observation of new_verbatim to 
+#' belong to a subject and to a class of sentiment
+
 analyse_new_verbatim<-function(new_verbatim){
     donnees<-data.frame(text=new_verbatim)
     sentiment<-model_sentiment$models
