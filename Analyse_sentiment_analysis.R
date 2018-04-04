c459cf178e5d86d4e3eeafb83ca432d00d7adaa8
statapp2017 <33500919+statapp2017@users.noreply.github.com>
2018-04-04 23:58:18 +0200

    Update Analyse_sentiment_analysis.R

--- Analyse_sentiment_analysis.R
+++ Analyse_sentiment_analysis.R
@@ -3,20 +3,20 @@ library(topicmodels)
 #' Gives the probabilities of each observation to be in a subject and in a class of 
 #' sentiment
 #' @param new_verbatim A list of new verbatims to analyse.
-#' @return A list which contains the probability of each observation of new_verbatim to 
+#' @return A list which contains the probability of each observation of \code{new_verbatim} to 
 #' belong to a subject and to a class of sentiment
 
-analyse_new_verbatim<-function(new_verbatim){
-    donnees<-data.frame(text=new_verbatim)
-    sentiment<-model_sentiment$models
-    params<-sentiment[[1]]$params
-    number_class<-params$num_class
-    result_preprocess<-preprocess_text(donnees,"text")
-    dtm<-gestion_dtm(result_preprocess$dtm,result_preprocess$tdm,nom_colonnes)
-    dtm_sentiment<-as.matrix(dtm)
-    prediction<-bagging_xgboost_prediction(dtm_sentiment,sentiment,number_class = number_class)
-    posterior_distribution <- posterior(model_theme$models,newdata= result_preprocess$dtm)
-    pos<-posterior_distribution$topics 
-    list(topics=pos,sentiment=prediction)
+analyse_new_verbatim <- function(new_verbatim) {
+  donnees <- data.frame(text = new_verbatim)
+  sentiment <- model_sentiment$models
+  params <- sentiment[[1]]$params
+  number_class <- params$num_class
+  result_preprocess <- preprocess_text(donnees, "text")
+  dtm <- gestion_dtm(result_preprocess$dtm, result_preprocess$tdm, nom_colonnes)
+  dtm_sentiment <- as.matrix(dtm)
+  prediction <- bagging_xgboost_prediction(dtm_sentiment,sentiment,number_class = number_class)
+  posterior_distribution <- posterior(model_theme$models, newdata = result_preprocess$dtm)
+  pos <- posterior_distribution$topics 
+  list(topics = pos, sentiment = prediction)
 }
 
