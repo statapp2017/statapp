5da635704aee3062a43ff53dcef087f78c6dfcca
statapp2017 <33500919+statapp2017@users.noreply.github.com>
2018-04-04 23:10:07 +0200

    Update Sentiment_visualisation.R

--- Sentiment_analysis/Sentiment_visualisation.R
+++ Sentiment_analysis/Sentiment_visualisation.R
@@ -1,7 +1,6 @@
 library(lime)
-setwd("D:/ENSAE/2emeannee/Statsapp")
-source("lime-master/R/explain.R")
-source("lime-master/R/dataframe.R")
+source("Lime modifiée/explain.R")
+source("Lime modifiée/dataframe.R")
 
 #' Gives a visual explanation on the verbatims classification. The algorithm
 #' is based on lime slightly modified to take into account the bagging
@@ -9,8 +8,13 @@ source("lime-master/R/dataframe.R")
 #' It's necessary to transform it with gestion_dtm before
 #' @param list_models The list of xgb.train models used for the prediction
 
-sentiments_results_visualisation<-function(new_data,list_models){
-  new_data<-as.data.frame(new_data)
+sentiments_results_visualisation<-function(new_verbatim,list_models){
+  donnees<-data.frame(text=new_verbatim)
+  sentiment<-model_sentiment$models
+  params<-sentiment[[1]]$params
+  number_class<-params$num_class
+  result_preprocess<-preprocess_text(donnees,"text")
+  new_data<-gestion_dtm(result_preprocess$dtm,result_preprocess$tdm,nom_colonnes)
   train_test<-list_models$train_test
   explanation<-vector("list",length(list_models$models))
   explainer_xgb <- lime(train_test[,2:ncol(train_test)],list_models$models,bin_continuous=FALSE)
