e4167c113b59435a6c3f85f01d652f4bfec27fe0
statapp2017 <33500919+statapp2017@users.noreply.github.com>
2018-04-05 09:38:00 +0200

    Update Models_xgboost.R

--- Sentiment_analysis/Models_xgboost.R
+++ Sentiment_analysis/Models_xgboost.R
@@ -4,27 +4,25 @@ library(tm)
 #' Returns a Document Term Matrix with the features used in the original model
 #' @param dtm  The dataframe of the Document Term Matrix to transform
 #' @param bigrams The dataframe of the bigrams Document Term Matrix 
-#' @param nom_colonnes Les features à garder dans la nouvelle dtm
+#' @param nom_colonnes The features to keep in the new dtm
 #' @return A new Document Term Matrix with the right features 
 
 gestion_dtm<-function(dtm,bigrams,nom_colonnes){
-  dtm<-as.data.frame(as.matrix(dtm))
+  dtm <- as.data.frame(as.matrix(dtm))
   bigrams<-as.data.frame(as.matrix(bigrams))
-  dtm<-cbind(dtm,bigrams)
+  dtm <- cbind(dtm,bigrams)
   a_enlever<-colnames(dtm)[which(!colnames(dtm) %in% nom_colonnes)]
   a_rajouter<-nom_colonnes[which(!nom_colonnes %in% colnames(dtm))]
-  if(length(a_enlever)>1){
+  if(length(a_enlever)>1) {
     dtm$mots_supplementaire<-rowSums(dtm[,colnames(dtm) %in% a_enlever])
     dtm<-dtm[,!colnames(dtm) %in% a_enlever]
-  }
-  else{
+  } else {
     dtm$mots_supplementaire<-rep(0,nrow(dtm))
   }
-  for (mots in a_rajouter){
-    dtm[,mots]<-rep(0,nrow(dtm))
+  for (mots in a_rajouter) {
+    dtm[,mots] <- rep(0,nrow(dtm))
   }
-  dtm<-dtm[,order(colnames(dtm))]
-  dtm
+  dtm[, order(colnames(dtm))]
 }
 
 #' Gives a list of number_models models and the place of their training observations
@@ -36,8 +34,8 @@ gestion_dtm<-function(dtm,bigrams,nom_colonnes){
 #' @return A list of xgb.train models and the place of the observations used to created them
 
 train_model_xgboost<-function(params,train_test,dvalid,number_models,train_test_split){
-  eta<-params$eta
-  params$objective<-"multi:softprob"
+  eta <- params$eta
+  params$objective <- "multi:softprob"
   models<-vector("list",number_models)
   for (i in 2:ncol(train_test)){
     train_test[,i]<-sapply(train_test[,i],as.numeric)
@@ -68,15 +66,15 @@ train_model_xgboost<-function(params,train_test,dvalid,number_models,train_test_
 #' the differents models, and the names of the features used in the models
 
 models_xgboost<-function(params,dtm_ep,data,column,number_class,number_models,train_test_split=.8,train_valid_split=.8){
-  params$num_class<-number_class
-  data_tot<-cbind(data[,column],as.data.frame(as.matrix(dtm_ep)))
-  colnames(data_tot)<-c("categorie",colnames(dtm_ep))
-  data_tot$categorie<-cut(data_tot$categorie,breaks=number_class,labels=as.factor(0:(number_class-1)))
-  data_tot[is.na(data_tot$categorie),"categorie"]<-0
-  sample1 = sample.int(n = nrow(data_tot), size = floor(train_valid_split*nrow(data_tot)), replace = F)
-  train_test<-data_tot[sample1,]
-  valid<-data_tot[-sample1,] 
-  dvalid<-xgb.DMatrix(data = as.matrix(valid[,2:ncol(data_tot)]),label = as.matrix(valid[,1]))
-  mod<-train_model_xgboost(params,train_test,dvalid,number_models,train_test_split)
-  list(models=mod$models,dvalid=valid,place=mod$entrainement,notes=data_tot[-sample1,1],train_test=train_test,nom_colonne=colnames(dtm_ep))
+  params$num_class <- number_class
+  data_tot<-cbind(data[,column], as.data.frame(as.matrix(dtm_ep)))
+  colnames(data_tot) <- c("categorie", colnames(dtm_ep))
+  data_tot$categorie <- cut(data_tot$categorie, breaks = number_class, labels = as.factor(0:(number_class-1)))
+  data_tot[is.na(data_tot$categorie), "categorie"] <- 0
+  sample1 <- sample.int(n = nrow(data_tot), size = floor(train_valid_split*nrow(data_tot)), replace = F)
+  train_test <- data_tot[sample1,]
+  valid <- data_tot[- sample1, ] 
+  dvalid <- xgb.DMatrix(data = as.matrix(valid[,2:ncol(data_tot)]),label = as.matrix(valid[,1]))
+  mod <- train_model_xgboost(params, train_test, dvalid, number_models, train_test_split)
+  list(models = mod$models, dvalid=valid,place=mod$entrainement,notes=data_tot[-sample1,1],train_test=train_test,nom_colonne=colnames(dtm_ep))
 }
