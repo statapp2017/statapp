818d804fe5fa4c925e4d40cf5e11e5445a2271be
statapp2017 <33500919+statapp2017@users.noreply.github.com>
2018-04-09 01:33:04 +0200

    Update 3_Creation dico.R

--- Preprocessing/3_Creation dico.R
+++ Preprocessing/3_Creation dico.R
@@ -1,20 +1,27 @@
+#' 
+#' 
+#' @param table_tm 
+#' @return 
+#' @examples
 
 creation_dico <- function(table_tm) {
-  corpus <- VCorpus(VectorSource(table_tm), readerControl=list(reader=readPlain, language="fr"))%>%
-    tm_map(content_transformer(removeNumbers))%>%tm_map(content_transformer(removePunctuation))%>%
+  # Suppress numbers, punctuation, etc from table_tm.
+  corpus <- VCorpus(VectorSource(table_tm), readerControl = list(reader = readPlain, language="fr")) %>%
+    tm_map(content_transformer(removeNumbers)) %>% tm_map(content_transformer(removePunctuation)) %>%
     tm_map(content_transformer(stripWhitespace))
-  dtm <- DocumentTermMatrix(corpus,control = list(weighting = weightTf))$dimnames
+  dtm <- DocumentTermMatrix(corpus, control = list(weighting = weightTf))$dimnames
   dico <- data.frame(mots = dtm$Terms)
+  # Stemming of words with hunspell.
   stemming <- function(word) {
-    stemm <- hunspell_stem(as.character(word), dict = dictionary("fr"))[[1]]
-    if (identical(stemm, character(0))) {
-      stemm <- as.character(word)
-    } else {
-      stemm <- stemm[1]
+    stem <- hunspell_stem(as.character(word), dict = dictionary("fr"))[[1]] # Possibilities of stem.
+    if (identical(stem, character(0))) { # If the stem is not in the dictionary.
+      stem <- as.character(word)
+    } else { # If there are possible stems in the dictionary
+      stem <- stem[1]
     }
-    stemm
+    stem
   }
-  dico$stem <- sapply(dico$mots, FUN=stemming)
+  dico$stem <- sapply(dico$mots, FUN = stemming)
   dico
 }
 
