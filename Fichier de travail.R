55abedaa196898abaf2ff7522049b761a0d2bddb
statapp2017 <33500919+statapp2017@users.noreply.github.com>
2018-04-09 01:12:59 +0200

    Update Fichier de travail.R

--- Fichier de travail.R
+++ Fichier de travail.R
@@ -2,24 +2,24 @@ rm(list = ls())
 
 ### Change here ###
 setwd("/Users/okabeshu/Documents/ENSAE/StatApp") # Path of the main folder
-
-name_data <- "verbatims.csv"
+name_data <- "verbatims.csv" # Name of the file
 
 ### Import data ###
 verbatim <- read.csv2(name_data, encoding = "latin1")
 epargne <- subset(verbatim, REPRISE_ACTIVITE == "Epargne")
-prevoyance<-subset(verbatim, REPRISE_ACTIVITE == "Prévoyance")
+prevoyance <- subset(verbatim, REPRISE_ACTIVITE == "Prévoyance")
 nrow(epargne)
 nrow(prevoyance)
 dico <- read.table("liste_francais.txt", encoding = "Latin1")
 model_theme <- readRDS("model_theme.RDS")
-parameter<-read.csv2("logloss.csv")
-params<-parameter[parameter$logloss==max(parameter$logloss),2:ncol(parameter)]
-colonne<-"recommandation_SGK"
-number_models<-20
-number_class<-3
-dtm<-readRDS("dtm_ep.RDS")
-dtm_tot<-readRDS("dtm_tot.RDS")
+parameter <- read.csv2("logloss.csv")
+params <- parameter[parameter$logloss == max(parameter$logloss), 2:ncol(parameter)]
+colonne <- "recommandation_SGK"
+colonne_note <- "raisons_recommandation"
+number_models <- 20
+number_class <- 3
+dtm <- readRDS("dtm_ep.RDS")
+dtm_tot <- readRDS("dtm_tot.RDS")
 
 ### Import functions ###
 # Preprocessing
@@ -39,7 +39,7 @@ source("Topic Processing/4 Theme i.R")
 source("Topic Processing/5 Model Theme.R")
 source("Topic Processing/7 Visualisation.R")
 
-#Sentiment analysis
+# Sentiment analysis
 source("Analyse_new_verbatim.R")
 source("Sentiment_analysis/Bagging.R")
 source("Sentiment_analysis/Models_xgboost.R")
@@ -49,28 +49,31 @@ source("Sentiment_analysis/Sentiment_visualisation.R")
 ### Test ###
 # Preprocessing
 describe_corpus(verbatim)
-par(mfrow = c(1,1), bg="beige")
-compar(prevoyance, "raisons_recommandation", dtm)
+par(mfrow = c(1, 1), bg = "beige")
+compar(prevoyance, colonne_note, dtm)
 
 # Create DTM
 #dtm_test <- preprocess_text(epargne, "raisons_recommandation")
 #dtm <- dtm_test$dtm
 
 # Topic Processing
-model_theme <- give_theme(dtm)
+model_theme <- give_theme(dtm) # Compute the model with LDA
 visualise_LDA(model_theme, dtm) # Visualisation
-nuage_de_mots(dtm)
+nuage_de_mots(dtm) # Plot the wordcloud
 try_tsne(model_theme$phi)
-communaute_freq(80,dtm)
-model_sentiment<-models_xgboost(params,dtm_tot,prevoyance,colonne,number_class,number_models)
-nom_colonnes<-model_sentiment$nom_colonne
-dvalid<-model_sentiment$dvalid
-prediction<-bagging_xgboost_prediction(as.matrix(dvalid[,2:ncol(dvalid)]),model_sentiment$models,number_class)
-plot_confusion_matrix(make_prediction(prediction),model_sentiment$notes)
+communaute_freq(80, dtm) # Plot the community of words
+
+# Sentiment Analysis
+model_sentiment <- models_xgboost(params,dtm_tot,prevoyance,colonne,number_class,number_models)
+nom_colonnes <- model_sentiment$nom_colonne
+dvalid <- model_sentiment$dvalid
+prediction <- bagging_xgboost_prediction(as.matrix(dvalid[,2:ncol(dvalid)]),model_sentiment$models,number_class)
+plot_confusion_matrix(make_prediction(prediction), model_sentiment$notes) # Confusion matrix
 measure_quality(prediction,model_sentiment$notes)
-analyse_new_verbatim(epargne[3:5,"raisons_recommandation"])
-importance<-bagging_xgboost_importance(model_sentiment,dtm_tot)
-sentiments_results_visualisation(epargne[3:5,"raisons_recommandation"],model_sentiment)
-xgb.ggplot.importance(as.data.table(importance),top_n = 50)
-a_eliminer<-importance$Feature[1:50]
-caracterise(model_theme$phi_t,a_eliminer)
+
+analyse_new_verbatim(epargne[3:5, colonne_note])
+importance <- bagging_xgboost_importance(model_sentiment, dtm_tot)
+sentiments_results_visualisation(epargne[3:5, colonne_note], model_sentiment)
+xgb.ggplot.importance(as.data.table(importance), top_n = 50)
+a_eliminer <- importance$Feature[1:50]
+caracterise(model_theme$phi_t, a_eliminer)
