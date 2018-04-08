fb9dc45e3e732a556296283933875f3a8884c538
statapp2017 <33500919+statapp2017@users.noreply.github.com>
2018-04-09 01:22:32 +0200

    Update 2_Spell checking.R

--- Preprocessing/2_Spell checking.R
+++ Preprocessing/2_Spell checking.R
@@ -13,13 +13,13 @@ spell_checker <- function(sentence) {
   # Tokenization of the sentence 
   list_words <- tokenize_words(as.character(gsub(" \n", ". ", sentence)), lowercase = TRUE)[[1]]
   correct_string <- ""
-  for (x in list_words) {
+  for (word in list_words) {
     # Check if the spelling of the words is correct.
-    if (hunspell_check(x, dict = dictionary("fr"))) {
-      correct_string <- paste(correct_string,x)
+    if (hunspell_check(word, dict = dictionary("fr"))) {
+      correct_string <- paste(correct_string, word)
     } else {
-      # If the spelling isn't accurate, we recover hunspell suggestions.
-      suggestion <- hunspell_suggest(x, dict = dictionary("fr"))[[1]]
+      # If the spelling is not accurate, we recover hunspell suggestions.
+      suggestion <- hunspell_suggest(word, dict = dictionary("fr"))[[1]]
       stop <- TRUE
       i <- 1
       while (stop & i < 3) {
@@ -31,9 +31,9 @@ spell_checker <- function(sentence) {
           i <- i + 1
         }
       }
-      # If the algorithm isn't able to find a good spelling, we leave it as it was.
+      # If the algorithm is not able to find a good spelling, we leave it as it was.
       if (i == 5) {
-        correct_string <- paste(correct_string, x)
+        correct_string <- paste(correct_string, word)
       }
     }  
   }
