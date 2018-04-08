29a9654c90a15bd45de7a59435b03e5a7c56eebe
statapp2017 <33500919+statapp2017@users.noreply.github.com>
2018-04-09 01:47:20 +0200

    Update 5_Create DTM.R

--- Preprocessing/5_Create DTM.R
+++ Preprocessing/5_Create DTM.R
@@ -2,18 +2,18 @@ library(tm)
 library(magrittr)
 library(NLP)
 
-#' Delete all the numbers, spaces and ponctuations present in corpus.
+#' Delete all the numbers, spaces, punctuations, and special characters present in the corpus.
 #' 
 #' @param corpus A collection of documents containing text (obtained with Corpus).
-#' @return A new corpus without the numbers, the spaces and the ponctuations.
+#' @return A new corpus without numbers, spaces, punctuations, and special characters.
 #' @examples notation_harmonisation(dataframe_corrige$lemme)
 
-notation_harmonisation<-function(table_tm){
-  table_tm<-str_replace_all(table_tm, "[AÁÀÂÄÃÅáàâäãå]", "a")%>%str_replace_all("[EÉÈÊËéèêë]", "e")%>%
-    str_replace_all("[IÍÏÎÌíìîï]", "i")%>%str_replace_all("[NÑñ]", "n")%>%str_replace_all("[OÓÒÔÖÕóòôöõ]", "o")%>%
-    str_replace_all("[UÚÙÛÜúùûü]", "u")%>%str_replace_all("[YÝýÿ]", "y")
-  corpus <- VCorpus(VectorSource(table_tm), readerControl=list(reader=readPlain, language="fr"))%>%
-  tm_map( content_transformer(removeNumbers))%>%tm_map(content_transformer(removePunctuation))%>%
+notation_harmonisation <- function(table_tm) {
+  table_tm <- str_replace_all(table_tm, "[AÁÀÂÄÃÅáàâäãå]", "a") %>% str_replace_all("[EÉÈÊËéèêë]", "e") %>%
+    str_replace_all("[IÍÏÎÌíìîï]", "i") %>% str_replace_all("[NÑñ]", "n") %>% str_replace_all("[OÓÒÔÖÕóòôöõ]", "o") %>%
+    str_replace_all("[UÚÙÛÜúùûü]", "u") %>% str_replace_all("[YÝýÿ]", "y")
+  corpus <- VCorpus(VectorSource(table_tm), readerControl = list(reader=readPlain, language = "fr")) %>%
+  tm_map(content_transformer(removeNumbers)) %>% tm_map(content_transformer(removePunctuation)) %>%
   tm_map(content_transformer(stripWhitespace))
   corpus
 }
@@ -24,9 +24,9 @@ notation_harmonisation<-function(table_tm){
 #' @return A new corpus without the stopwords.
 #' @examples delete_stopwords(notation_harmonisation(table_tm)) 
 
-delete_stopwords<-function(corpus){
-  stopwords_plus<-c("ete","etre","avoir","j","m","car","donc","encore","avoir","etre","parce","a")
-  myStopWords<-c(c(stopwords("fr")),stopwords_plus)
+delete_stopwords <- function(corpus) {
+  stopwords_plus <- c("ete","etre","avoir","j","m","car","donc","encore","avoir","etre","parce","a")
+  myStopWords <- c(c(stopwords("fr")), stopwords_plus)
   corpus <- tm_map(corpus, removeWords, myStopWords)
   corpus
 }
@@ -53,7 +53,7 @@ bigramTokenizer <- function(word_collection) {
 
 #' Given a data containing sentences in \code{column}, give the Document-Term Matrix
 #' after text transformations (spell correction, lemmatization, removal 
-#' of stopwords, ponctuations etc...). Preprocessing of the database.
+#' of stopwords, punctuations etc...). Preprocessing of the database.
 #' 
 #' @param data A dataframe containing the sentences.
 #' @param column The column of the dataframe containing the sentences to work on.
@@ -62,14 +62,14 @@ bigramTokenizer <- function(word_collection) {
 #' @return Both the Document Term Matrix corresponding to the given sentences and
 #' a new dataframe containing the lemmatisation of the sentences after spell correction.
 
-preprocess_text<-function(data,column,sparseness=0.99){
-  data$corrige<-sapply(X=data[,column],FUN=spell_checker)
-  dico<-creation_dico(data$corrige)
-  dataframe_corrige <- lemmatizer_dataframe(data,dico,column)
-  table_tm<- dataframe_corrige$lemme
-  corpus <-notation_harmonisation(table_tm)%>%delete_stopwords()
-  dtm <- creation_DTM(corpus,sparseness)
-  tdm <- DocumentTermMatrix(corpus, control = list(tokenize = BigramTokenizer))
-  tdm<-removeSparseTerms(tdm,0.8)
-  list(dtm=dtm,dataframe_corrige=dataframe_corrige,tdm=tdm)
+preprocess_text <- function(data, column, sparseness = 0.99) {
+  data$corrige <- sapply(X = data[, column], FUN = spell_checker) # Spell checking
+  dico <- creation_dico(data$corrige)
+  dataframe_corrige <- lemmatizer_dataframe(data, dico, column) # Lemmatisation
+  table_tm <- dataframe_corrige$lemme
+  corpus <- notation_harmonisation(table_tm) %>% delete_stopwords() # Suppress useless elements
+  dtm <- creation_DTM(corpus, sparseness) # Creation of the Document Term Matrix
+  tdm <- DocumentTermMatrix(corpus, control = list(tokenize = BigramTokenizer)) # DTM for bigrams
+  tdm <- removeSparseTerms(tdm, 0.8)
+  list(dtm = dtm, dataframe_corrige = dataframe_corrige, tdm = tdm)
 }
