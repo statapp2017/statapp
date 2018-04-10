8ca869f679df0c393c8e4f4bdae3946ddb4883ea
statapp2017 <33500919+statapp2017@users.noreply.github.com>
2018-04-11 01:03:33 +0200

    Update 7 Visualisation.R

--- Topic Processing/7 Visualisation.R
+++ Topic Processing/7 Visualisation.R
@@ -1,6 +1,6 @@
 library(LDAvis)
 
-#' Visualise dynamically the results of the topic processing with LDA thanks to LDAvis.
+#' Visualises dynamically the results of the topic processing with LDA thanks to LDAvis.
 #' 
 #' @param model The LDA model, output of \code{\link{give_theme}}.
 #' @param dtm The document-term matrix obtained after the preprocessing.
@@ -8,18 +8,14 @@ library(LDAvis)
 #' @examples
 #' visualise_LDA(model_theme, dtm_ep)
 
-
 visualise_LDA <- function(model, dtm) {
   #model <- give_theme(dtm)
-  phi <- model$phi_t # Probability that a word belongs to a topic (ok)
-  theta <- model$theta # Probability that a document belongs to a topic (ok)
-  doc.length <- as.data.frame(table(dtm$i))[2]$Freq # Number of words per document (ok)
+  phi <- model$phi_t # Probability that a word belongs to a topic 
+  theta <- model$theta # Probability that a document belongs to a topic 
+  doc.length <- as.data.frame(table(dtm$i))[2]$Freq # Number of words per document 
   vocab <- dtm$dimnames$Terms # Words list
-  #vocab[92] <- "fa"
-  #vocab[93] <- "re"
-  term.frequency <- as.data.frame(table(dtm$j))[2]$Freq # Fréquence des mots dans tout le document"
+  term.frequency <- as.data.frame(table(dtm$j))[2]$Freq # Frequency of words in the whole document
   json <- createJSON(phi = phi, theta = theta, doc.length = doc.length, vocab = vocab, 
                      term.frequency = term.frequency)
   serVis(json, out.dir = "vis", open.browser = TRUE)
 }
-
