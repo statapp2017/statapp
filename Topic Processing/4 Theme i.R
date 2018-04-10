f6275eb5d6d6aea5ff29b13693c34e6963e48279
statapp2017 <33500919+statapp2017@users.noreply.github.com>
2018-04-11 01:01:22 +0200

    Update 4 Theme i.R

--- Topic Processing/4 Theme i.R
+++ Topic Processing/4 Theme i.R
@@ -1,37 +1,37 @@
 library(plotly)
 library(ggplot2)
 
-## ANALYSE THEMATIQUE ##
-# Characterize the i-th topic
-# a_eliminer= the words associated with a strong sentiment by xgboost
+#' Characterises the i-th topic.
+#' 
+#' @param phi_t The distribution of words by documents (matrix)
+#' @param a_eliminer The words associated with a strong sentiment by xgboost
 
-caracterise<-function(phi_t,a_eliminer){
+caracterise <- function(phi_t, a_eliminer) {
   df <- as.data.frame(t(phi_t))
-  df$nom<-rownames(df)
-  col<-list()
-  for (i in (1:(ncol(df)-1))){
-    titre <-paste("Theme",as.character(i),sep="")
-    col[i]<-titre
+  df$nom <- rownames(df)
+  col <- list()
+  for (i in (1:(ncol(df) - 1))) {
+    titre <- paste("Theme", as.character(i), sep = "")
+    col[i] <- titre
   }
-  col[length(col)+1]<-"nom"
-  colnames(df)<-col
-  df<- df[!df$nom %in% a_eliminer,]
-  a_garder<-c()
-  for (i in (1:(ncol(df)-1))){
-    df<-df[order(df[,i],decreasing=T),]
-    a_garder<-c(a_garder,c(rownames(df[1:10,])))
+  col[length(col) + 1] <- "nom"
+  colnames(df) <- col
+  df <- df[!df$nom %in% a_eliminer, ]
+  a_garder <- c()
+  for (i in (1:(ncol(df) - 1))) {
+    df <- df[order(df[,i], decreasing = T), ]
+    a_garder <- c(a_garder, c(rownames(df[1:10, ])))
     ref <- df[,i]
-    for (j in 1:10){
-    df$theme[j]<-paste("Theme",as.character(i))
-    df$valeur[j]<-ref[j]
+    for (j in 1:10) {
+    df$theme[j] <- paste("Theme", as.character(i))
+    df$valeur[j] <- ref[j]
     }
   }
-  df<-df[a_garder,]
-  df <- df[order(df[,which(colnames(df)=="theme")],decreasing=F),]
-  df$nom<-factor(df$nom, levels=df$nom)
-  p<-ggplot(data=df,aes(x=nom,y=valeur,color=theme)) +
-    geom_bar(stat="identity")+labs(title="Mots illustrant les différents themes",
-                                  x ="Mots", y = "Probabilité")+theme(axis.text.x = element_text(angle=45))
-  p1<-ggplotly(p)
-  print(p1)
+  df <- df[a_garder, ]
+  df <- df[order(df[, which(colnames(df) == "theme")], decreasing = F), ]
+  df$nom <- factor(df$nom, levels = df$nom)
+  p <- ggplot(data = df, aes(x = nom, y = valeur, color = theme)) +
+    geom_bar(stat = "identity") + labs(title = "Mots illustrant les différents thèmes",
+                                  x = "Mots", y = "Probabilité") + theme(axis.text.x = element_text(angle = 45))
+  print(ggplotly(p))
 }
