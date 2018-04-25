884833bc8c18744edc4f3dcd53d6cde56033290c
statapp2017 <33500919+statapp2017@users.noreply.github.com>
2018-04-25 15:55:56 +0200

    Update Quality_measurement.R

--- Sentiment_analysis/Quality_measurement.R
+++ Sentiment_analysis/Quality_measurement.R
@@ -5,13 +5,13 @@ library(pROC)
 #' Gives the multiclass.roc associated with our model and plots the error of the model.
 #' 
 #' @param prediction The dataframe of probability associating each document 
-#' to a class of sentiment
+#' to a class of sentiment.
 #' @param vecteur_note The real class of the documents
 #' @param nom_colonnes The features to keep in the new dtm
 
 measure_quality <- function(prediction, vecteur_note) {
   pred <- vector("list", nrow(prediction))
-  for (i in 1:nrow(prediction)){
+  for (i in 1:nrow(prediction)) {
     pred[i]<-(which(prediction[i, ] == max(prediction[i, ])) - 1)
   }
   pred <- unlist(pred)  
@@ -25,16 +25,17 @@ measure_quality <- function(prediction, vecteur_note) {
   print(graph)
 }
 
-#' Plots the confusion_matrix of the models.
+#' Plots the confusion matrix of the models.
 #' 
 #' @param pred The vecotr of predicted class
 #' @param vecteur_note The real class of the documents
+#' @return A plot of the confusion matrix of the models.
 
 plot_confusion_matrix <- function(pred, vecteur_note) {
   contingency <- table(pred, vecteur_note)
   donnees <- as.data.frame(expand.grid(sort(unique(pred)), sort(unique(vecteur_note))))
   donnees$contingency <- as.vector(contingency)
-  p <- plot_ly(donnees, x = ~Var1, y =~Var2,
+  p <- plot_ly(donnees, x = ~Var1, y = ~Var2,
                z = ~contingency, type = "heatmap") %>% 
     add_annotations(text = ~contingency, showarrow = FALSE, font = list(color = 'white', size = 30)) %>%
     layout(title = "Matrice de confusion", titlefont = list(size = 20),
